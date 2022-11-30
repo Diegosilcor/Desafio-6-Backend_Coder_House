2417e3e20edf8e9407ab7f624e00751c1ac43fae
Diegosilcor <diegosilvacordoba@hotmail.com>
2022-11-30 09:01:44 +0100

    Desafio 6

---
+++ src/contenedor.js
@@ -0,0 +1,133 @@
+const fs = require("fs");
+
+class Contenedor {
+  constructor(fileName) {
+    this._filename = fileName;
+    this._readFileOrCreateNewOne();
+  }
+
+  async _readFileOrCreateNewOne() {
+    try {
+      await fs.promises.readFile(this._filename, "utf-8");
+    } catch (error) {
+      error.code === "ENOENT"
+        ? this._createEmptyFile()
+        : console.log(
+            `Error de codigo: ${error.code} |Error en la lectura del archivo 🚫 ${this._filename}`
+          );
+    }
+  }
+
+  async _createEmptyFile() {
+    fs.writeFile(this._filename, "[]", (error) => {
+      error
+        ? console.log(error)
+        : onsole.log(`Archivo ${this._filename} creado, ya que no existia en el sistema 🚫`);
+    });
+  }
+
+  async getById(id) {
+    id = Number(id);
+    try {
+      const data = await this.getData();
+      const parsedData = JSON.parse(data);
+
+      return parsedData.find((producto) => producto.id === id);
+    } catch (error) {
+      console.log(
+        `Error de codigo: ${error.code} | Hubo un error al intentar eliminar un elemento por su ID 🚫 (${id})`
+      );
+    }
+  }
+
+  async deleteById(id) {
+    try {
+      id = Number(id);
+      const data = await this.getData();
+      const parsedData = JSON.parse(data);
+      const objectIdToBeRemoved = parsedData.find(
+        (producto) => producto.id === id
+      );
+
+      if (objectIdToBeRemoved) {
+        const index = parsedData.indexOf(objectIdToBeRemoved);
+        parsedData.splice(index, 1);
+        await fs.promises.writeFile(this._filename, JSON.stringify(parsedData));
+        return true;
+      } else {
+        console.log(`ID ${id} no existe en el archivo`);
+        return null;
+      }
+    } catch (error) {
+      console.log(
+        `Error de codigo: ${error.code} | Hubo un error al intentar eliminar un elemento por su ID (${id})`
+      );
+    }
+  }
+
+  async updateById(id, newData) {
+    try {
+      id = Number(id);
+      const data = await this.getData();
+      const parsedData = JSON.parse(data);
+      const objectIdToBeUpdated = parsedData.find(
+        (producto) => producto.id === id
+      );
+      if (objectIdToBeUpdated) {
+        const index = parsedData.indexOf(objectIdToBeUpdated);
+        const {title, price, thumbnail} = newData;
+
+        parsedData[index]['title'] = title;
+        parsedData[index]['price'] = price;
+        parsedData[index]['thumbnail'] = thumbnail;
+        await fs.promises.writeFile(this._filename, JSON.stringify(parsedData));
+        return true;
+      } else {
+        `Error de codigo: ${error.code} | Hubo un error al intentar actualizar un elemento por su ID (${id})`
+        return null;
+      }
+
+    } catch (error) {
+      `Error de codigo: ${error.code} | Hubo un error al intentar actualizar un elemento por su ID (${id})`
+    }
+  }
+
+  async save(object) {
+    try {
+      const allData = await this.getData();
+      const parsedData = JSON.parse(allData);
+
+      object.id = parsedData.length + 1;
+      parsedData.push(object);
+
+      await fs.promises.writeFile(this._filename, JSON.stringify(parsedData));
+      return object.id;
+    } catch (error) {
+      console.log(
+        `Error de codigo: ${error.code} | Hubo un error al intentar guardar su archivo 🚫`
+      );
+    }
+  }
+
+  async deleteAll() {
+    try {
+      await this._createEmptyFile();
+    } catch (error) {
+      console.log(
+        `Hubo un error (${error.code}) al intentar borrar los archivos 🚫`
+      );
+    }
+  }
+
+  async getData() {
+    const data = await fs.promises.readFile(this._filename, "utf-8");
+    return data;
+  }
+
+  async getAll() {
+    const data = await this.getData();
+    return JSON.parse(data);
+  }
+}
+
+module.exports = Contenedor;
